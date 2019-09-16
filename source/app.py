9fff30e87b5886c155e3264cdee8cbe442d700b0
Artem Kuchumov <duketemon@gmail.com>
2019-09-16 22:50:11 +0200

    Basic version: backend + frontend

---
+++ source/app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'files'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/save-record', methods=['POST'])
+def save_record():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    file_name = secure_filename(file.filename)
+    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(full_file_name)
+    return '<h1>Success</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
